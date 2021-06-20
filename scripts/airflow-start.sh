f99faa749de8d4395ae719307f705954b89ee284
dion-ricky <dionrickysptr@gmail.com>
2021-06-20 12:56:57 +0000

    Installed airflow

---
+++ scripts/airflow-start.sh
@@ -0,0 +1,6 @@
+# start the web server, default port is 8080
+
+
+# start the scheduler
+# open a new terminal or else run webserver with ``-D`` option to run it as a daemon
+airflow webserver --port 8080 & airflow scheduler
