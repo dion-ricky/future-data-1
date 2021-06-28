beedd9087964a36a3289d051c0ffd47e2756b219
dion-ricky <dionrickysptr@gmail.com>
2021-06-28 07:08:51 +0000

    Update Migration + ETL for Mart Customer Transaction Count

--- scripts/etls/mart_customer_transaction_count.sql
+++ scripts/etls/mart_customer_transaction_count.sql
@@ -1,11 +1,27 @@
+WITH order_date AS (
+	SELECT 
+		od.order_key AS order_key,
+		dd.date_id AS order_date 
+	FROM 
+		order_dim od 
+	LEFT JOIN
+		date_dim dd 
+	ON DATE(od.order_date) = dd.full_date
+)
+
 INSERT INTO public.mart_customer_transaction_count (
     user_name,
+	order_date,
     order_count
 )
-SELECT 
-	od.user_name,
+
+SELECT
+	COALESCE (od.user_name, 'All User') AS user_name,
+	COALESCE (odt.order_date, 0) AS order_date,
 	count(DISTINCT od.order_key) AS order_count
 FROM 
-	order_dim od 
-GROUP BY 1
-ORDER BY 2 DESC;
+	order_dim od
+LEFT JOIN
+	order_date odt
+ON od.order_key = odt.order_key
+GROUP BY CUBE(od.user_name, odt.order_date);
--- scripts/migrations/mart_customer_transaction_count.sql
+++ scripts/migrations/mart_customer_transaction_count.sql
@@ -3,5 +3,6 @@ DROP TABLE
 
 CREATE TABLE public.mart_customer_transaction_count (
     user_name varchar NULL,
+    order_date int8 NULL,
     order_count int8 NULL
 );
