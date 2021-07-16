75632a5884cff96e26e1c306efa6a18247fd5a4a
dion-ricky <dionrickysptr@gmail.com>
2021-07-16 12:48:44 +0000

    Update connection factory to use psycopg2

--- notebook/util/dbconn.py
+++ notebook/util/dbconn.py
@@ -1,13 +1,15 @@
-from sqlalchemy import create_engine
+import psycopg2
 
 DEFAULT_CONNECTION = {
     'password': 'qshOke46RvOg0',
-    'host': 'localhost',
+    'host': '35.184.46.9',
     'port': '8084',
-    'dbname': 'ecommerce'
+    'dbname': 'ecommerce',
+    'user': 'postgres'
 }
 
 
+# For sqlalchemy
 def get_conn_string(conn_dict=None):
     conn_dict = (conn_dict
                  if conn_dict is not None
@@ -23,9 +25,22 @@ def get_conn_string(conn_dict=None):
             + conn_dict['dbname'])
 
 
+def get_conn_dict(conn_dict=None):
+    conn_dict = (conn_dict
+                if conn_dict is not None
+                else DEFAULT_CONNECTION)
+    
+    return conn_dict
+
+
 class DbConn:
     def __init__(self, conn_dict=None):
-        self.connection_string = get_conn_string(conn_dict)
+        self.connection = get_conn_dict(conn_dict)
     
-    def get_engine(self):
-        return create_engine(self.connection_string)
+    def get_conn(self):
+        config = self.connection
+        return psycopg2.connect(dbname=config['dbname'],
+                                user=config['user'],
+                                password=config['password'],
+                                host=config['host'],
+                                port=config['port'])
