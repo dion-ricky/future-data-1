d1368c02b9553f6e299dca848b0589b521e537fe
dion-ricky <dionrickysptr@gmail.com>
2021-08-12 07:01:18 +0000

    [#29] Fact shipping cost

--- scripts/etls/fact_shipping_cost.sql
+++ scripts/etls/fact_shipping_cost.sql
@@ -1,28 +1,102 @@
-TRUNCATE public.fact_shipping_cost;
+TRUNCATE warehouse.fact_shipping_cost;
 
-INSERT INTO public.fact_shipping_cost (
-    pulau,
-    state_id,
-    "state",
-    city,
-    min_shipping_cost,
-    max_shipping_cost,
-    avg_shipping_cost
+INSERT INTO warehouse.fact_shipping_cost (
+	order_legacy_id,
+    order_date,
+    order_time,
+    product_sk,
+    seller_sk,
+    seller_location_sk,
+    customer_location_sk,
+	item_id,
+    price,
+    shipping_cost
 )
-SELECT 
-	tpp.pulau,
-	sd.seller_state_id AS state_id,
-	COALESCE (sd.seller_state, 'Semua Provinsi') AS "state",
-	COALESCE (sd.seller_city, 'Semua KabKo') AS city,
-	min(oid2.shipping_cost) AS min_shipping_cost,
-	max(oid2.shipping_cost) AS max_shipping_cost,
-	avg(oid2.shipping_cost) AS avg_shipping_cost
-FROM 
-	order_item_dim oid2
-LEFT JOIN
-	seller_dim sd
-ON oid2.seller_key = sd.seller_key 
-LEFT JOIN 
-	temp_provinsi_pulau tpp
-ON lower(trim(sd.seller_state)) = lower(trim(tpp.provinsi))
-GROUP BY 1, ROLLUP ((sd.seller_state_id, sd.seller_state), sd.seller_city);
+
+WITH order_date AS (
+SELECT
+	o.order_id,
+	dd.date_id AS order_date
+FROM
+	public."order" o
+LEFT JOIN warehouse.date_dim dd ON
+	date(o.order_date) = date(dd.full_date) ),
+order_time AS (
+SELECT
+	o.order_id,
+	td.time_id AS order_time
+FROM
+	public."order" o
+LEFT JOIN warehouse.time_dim td ON
+	o.order_date :: timestamp :: time = td."time" ),
+seller_location AS (
+SELECT
+	DISTINCT 
+	oi.seller_id,
+	ld.location_sk AS seller_location_sk
+FROM
+	public."order" o2
+LEFT JOIN public.order_item oi ON
+	o2.order_id = oi.order_id
+LEFT JOIN public.seller s ON
+	oi.seller_id = s.seller_id
+LEFT JOIN warehouse.location_dim ld ON
+	s.seller_zip_code = ld.zip_code
+	AND lower(s.seller_state) = lower(ld.state)
+		AND lower(s.seller_city) = lower(ld.city) ),
+customer_dedup AS (
+SELECT
+	user_name,
+	customer_zip_code,
+	customer_city,
+	customer_state
+FROM
+	(
+	SELECT
+		DISTINCT *,
+		RANK() OVER( PARTITION BY user_name
+	ORDER BY
+		customer_zip_code DESC) AS RANK
+	FROM
+		public.user u ) AS f
+WHERE
+	RANK = 1 ),
+customer_location AS (
+SELECT
+	o2.order_id,
+	ld.location_sk AS customer_location_sk
+FROM
+	public."order" o2
+LEFT JOIN customer_dedup cdp ON
+	o2.user_name = cdp.user_name
+LEFT JOIN warehouse.location_dim ld ON
+	cdp.customer_zip_code = ld.zip_code
+	AND lower(cdp.customer_state) = lower(ld.state)
+		AND lower(cdp.customer_city) = lower(ld.city) )
+SELECT
+	oi.order_id AS order_legacy_id,
+	odt.order_date,
+	ott.order_time,
+	pd.product_sk,
+	sd.seller_sk,
+	slc.seller_location_sk,
+	clc.customer_location_sk,
+	oi.order_item_id AS item_id,
+	oi.price,
+	oi.shipping_cost
+FROM
+	public.order_item oi
+LEFT JOIN order_date odt ON
+	oi.order_id = odt.order_id
+LEFT JOIN order_time ott ON
+	oi.order_id = ott.order_id
+LEFT JOIN seller_location slc ON
+	oi.seller_id = slc.seller_id
+LEFT JOIN public."order" o3 ON
+	oi.order_id = o3.order_id
+LEFT JOIN customer_location clc ON
+	o3.order_id = clc.order_id
+LEFT JOIN warehouse.product_dim pd ON
+	oi.product_id = pd.product_legacy_id
+LEFT JOIN warehouse.seller_dim sd ON
+	oi.seller_id = sd.seller_legacy_id;
--- scripts/migrations/fact_shipping_cost.sql
+++ scripts/migrations/fact_shipping_cost.sql
@@ -1,12 +1,23 @@
 DROP TABLE
-    IF EXISTS public.fact_shipping_cost;
+    IF EXISTS warehouse.fact_shipping_cost;
 
-CREATE TABLE public.fact_shipping_cost (
-    pulau varchar NULL,
-    state_id int8 NULL,
-    "state" varchar NULL,
-    city varchar NULL,
-    min_shipping_cost float8 NULL,
-    max_shipping_cost float8 NULL,
-    avg_shipping_cost float8 NULL
+CREATE TABLE warehouse.fact_shipping_cost (
+    order_legacy_id varchar,
+    order_date int4,
+    order_time int4,
+    product_sk int4,
+    seller_sk int4,
+    seller_location_sk int4,
+    customer_location_sk int4,
+    item_id int2,
+    price float8 null,
+    shipping_cost float8 null,
+    CONSTRAINT fact_shipping_cost_pkey PRIMARY KEY(order_legacy_id, order_date, order_time, product_sk, seller_sk, seller_location_sk, customer_location_sk, item_id)
 );
+
+ALTER TABLE warehouse.fact_shipping_cost ADD CONSTRAINT fss_order_date_fkey FOREIGN KEY (order_date) REFERENCES warehouse.date_dim(date_id);
+ALTER TABLE warehouse.fact_shipping_cost ADD CONSTRAINT fss_order_time_fkey FOREIGN KEY (order_time) REFERENCES warehouse.time_dim(time_id);
+ALTER TABLE warehouse.fact_shipping_cost ADD CONSTRAINT fss_product_sk_fkey FOREIGN KEY (product_sk) REFERENCES warehouse.product_dim(product_sk);
+ALTER TABLE warehouse.fact_shipping_cost ADD CONSTRAINT fss_seller_sk_fkey FOREIGN KEY (seller_sk) REFERENCES warehouse.seller_dim(seller_sk);
+ALTER TABLE warehouse.fact_shipping_cost ADD CONSTRAINT fss_seller_location_sk_fkey FOREIGN KEY (seller_location_sk) REFERENCES warehouse.location_dim(location_sk);
+ALTER TABLE warehouse.fact_shipping_cost ADD CONSTRAINT fss_customer_location_sk_fkey FOREIGN KEY (customer_location_sk) REFERENCES warehouse.location_dim(location_sk);
